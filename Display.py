2c06fd9810414fa11ccab0c96ecba7dde8391a30
Colton Gowans <coltongowans@gmail.com>
2022-11-19 13:43:39 -0700

    Trying a background image

--- Display.py
+++ Display.py
@@ -2,16 +2,19 @@ import tkinter as tk
 import time
 
 first_text = "Hello There!"
-light_blue = "00DEFF"
+light_blue = "#00DEFF"
+background_image = tk.PhotoImage(file="xp-hills.jpg")
 
 def displayTheScreen():
-    root = tk.Tk()
+    window = tk.Tk()
     
-    main_frame = tk.Frame(root, bg="blue", height=1080, width=1920)
+    main_frame = tk.Frame(window, bg=light_blue, height=1080, width=1920)
     main_frame.pack(fill="both", expand="true")
+    window.geometry("1920x1080")
 
-    root.geometry("1920x1080")
+    label_image = tk.Label(window, i=background_image)
+    label_image.pack()
 
     # first_text = tk.Label(root, text=first_text, font="")
 
-    root.mainloop()
+    window.mainloop()
