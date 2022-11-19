3c315d88738d1a487c468a39c2ee590f9f2fd977
Colton Gowans <coltongowans@gmail.com>
2022-11-19 11:20:48 -0700

    Setting up initial logic

---
+++ main.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import time
+
+
+
+def checkForMotion():
+    print("checking for motion")
+    return True
+
+def onWake():
+    print("sensor detected motion! Wake the screen!")
+
+def displayTheScreen():
+    root = tk.Tk()
+
+
+
+    root.mainloop()
+
+def main():
+
+    # Continous loop to wait for motion to be detected
+    checkForMotion()
+    # Once we have motion, go into initial procedure to wake up the screen
+    onWake()
+
+    #Create the UI to display for the user
+    displayTheScreen()
+    
+
+if __name__ == '__main__':
+    main()
