b914a8e2b9b39fb2f3ad078a143b1bd89260e995
Marcus Kittelson <marcus.kittelson@ucalgary.ca>
2022-11-19 14:23:58 -0700

    Image displays nicely

--- Display.py
+++ Display.py
@@ -1,16 +1,19 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 import time
 
 first_text = "Hello There!"
 light_blue = "#00DEFF"
-background_image = tk.PhotoImage(file="xp-hills.jpg")
 
 def displayTheScreen():
     window = tk.Tk()
     
-    main_frame = tk.Frame(window, bg=light_blue, height=1080, width=1920)
-    main_frame.pack(fill="both", expand="true")
-    window.geometry("1920x1080")
+    # main_frame = tk.Frame(window, bg=light_blue, height=1080, width=1920)
+    # main_frame.pack(fill="both", expand="true")
+    # window.geometry("1920x1080")
+
+    print("")
+    background_image = ImageTk.PhotoImage(Image.open("/home/pi/Documents/Coding Projects/Hack-The-Change/xp-hills.png"))
 
     label_image = tk.Label(window, i=background_image)
     label_image.pack()
