04774c5105dce17adea6656523af065c7346896f
Colton Gowans <coltongowans@gmail.com>
2022-11-19 14:59:50 -0700

    updating frame

--- FirstScreen.py
+++ FirstScreen.py
@@ -6,17 +6,20 @@ first_text = "Hello There!"
 light_blue = "#00DEFF"
 
 def displayScreenOne(window):
+    window.geometry("1920x1080")
     
-    # main_frame = tk.Frame(window, bg=light_blue, height=1080, width=1920)
+    main_frame = tk.Frame(window, height=1080, width=1920)
     # main_frame.pack(fill="both", expand="true")
-    # window.geometry("1920x1080")
+    main_frame.pack()
+    main_frame.place(anchor='center', relx=0.5, rely=0.5)
 
-    background_image = ImageTk.PhotoImage(Image.open("/home/pi/Documents/Coding Projects/Hack-The-Change/xp-hills.png"))
 
-    bg_image = tk.Label(window, i=background_image)
-    bg_image.pack()
+    bg_image = ImageTk.PhotoImage(Image.open("/home/pi/Documents/Coding Projects/Hack-The-Change/xp-hills.png"))
 
-    welcome_text = tk.Label(window, text="Hey You!", font="Courier", height=100, width=200, bg="black", fg="white")
-    welcome_text.place(x=0, y=0)
+    background = tk.Label(window, i=bg_image)
+    background.pack()
+
+    # welcome_text = tk.Label(window, text="Hey You!", font="Courier", height=100, width=200, bg="black", fg="white")
+    # welcome_text.place(x=0, y=0)
 
     # first_text = tk.Label(root, text=first_text, font="")
