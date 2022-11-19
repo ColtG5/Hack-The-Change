7c1281e57bffba5c43ebd4541a50271aaa67c372
Marcus Kittelson <marcus.kittelson@ucalgary.ca>
2022-11-19 13:20:21 -0700

    Barebones of the first display

--- Display.py
+++ Display.py
@@ -7,11 +7,11 @@ light_blue = "00DEFF"
 def displayTheScreen():
     root = tk.Tk()
     
-    main_frame = tk.Frame(root, bg=light_blue, height=1080, width=1920)
+    main_frame = tk.Frame(root, bg="blue", height=1080, width=1920)
     main_frame.pack(fill="both", expand="true")
 
     root.geometry("1920x1080")
 
-    first_text = tk.Label(root, text=first_text, font="")
+    # first_text = tk.Label(root, text=first_text, font="")
 
     root.mainloop()
