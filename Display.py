98aff6140f34cec50cbedc69e7b0a09fe4add26f
Colton Gowans <coltongowans@gmail.com>
2022-11-19 13:12:25 -0700

    Starting to get first window created

--- Display.py
+++ Display.py
@@ -1,9 +1,17 @@
 import tkinter as tk
 import time
 
+first_text = "Hello There!"
+light_blue = "00DEFF"
+
 def displayTheScreen():
     root = tk.Tk()
+    
+    main_frame = tk.Frame(root, bg=light_blue, height=1080, width=1920)
+    main_frame.pack(fill="both", expand="true")
 
+    root.geometry("1920x1080")
 
+    first_text = tk.Label(root, text=first_text, font="")
 
     root.mainloop()
