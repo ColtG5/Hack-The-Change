707566cfb035288d2cc68b6a41de9084486e8b6b
Colton Gowans <coltongowans@gmail.com>
2022-11-20 10:34:59 -0700

    cleaning up unused files, commented lines of code

--- SwitchingScreens.py
+++ SwitchingScreens.py
@@ -7,10 +7,7 @@ TIMING_INTRO = 3
 TIMING_PIC1 = 8.5
 TIMING_PIC2 = 9
 
-continue_program = True
-
 def displayAndSwitchWindows(window):
-    print("testing commit")
 
     window.geometry("1920x1080")
     window.resizable(0,0)
@@ -21,18 +18,11 @@ def displayAndSwitchWindows(window):
     main_frame = tk.Frame(window, height=1080, width=1920, bg="black")
     main_frame.pack(fill="both", expand="true")
     window.update()
-    # main_frame.pack(side=tk.TOP)
-    # main_frame.place(anchor='center', relx=0.5, rely=0.5)
 
-    while continue_program:
+    while True:
 
-        # blank = tk.Label(main_frame, bg="black")
-
-        # Continous loop to wait for motion to be detected
         print("checking for motion")
         ElevatorReduction.sensor()
-        # # Once we have motion, go into initial procedure to wake up the screen
-        # onWake()
 
         intro_image = ImageTk.PhotoImage(Image.open("1.png"))
         intro = tk.Label(main_frame, i=intro_image)
@@ -63,34 +53,7 @@ def displayAndSwitchWindows(window):
         background2.destroy()
         window.update()
 
-    # window.update()
-    # window.quit()
-    # window.upadte()
-
 def keyPressed(event, window):
-    # print(event)
-    # print(window)
-    # window.update()
 
     window.destroy()
     window.update()
-
-    # continue_program = False
-
-# def displayWindowTwo():
-#     window = tk.Tk()
-#     window.geometry("1920x1080")
-#     window.resizable(0,0)
-    
-#     main_frame = tk.Frame(window, height=1080, width=1920, bg="black")
-#     main_frame.pack(fill="both", expand="true")
-#     # main_frame.pack(side=tk.TOP)
-#     # main_frame.place(anchor='center', relx=0.5, rely=0.5)
-
-#     bg_image = ImageTk.PhotoImage(Image.open("man-waving.png"))
-#     background = tk.Label(main_frame, i=bg_image)
-#     background.pack()
-
-#     window.mainloop()
-#     time.sleep(1.5)
-#     window.withdraw()
--- main.py
+++ main.py
@@ -1,48 +1,11 @@
 import tkinter as tk
-import time
-import ElevatorReduction
-from ElevatorReductionWindows import *
-from FrameSwitcher import *
 from SwitchingScreens import *
-
-def checkForMotion():
-    ElevatorReduction.sensor()
-    return
-
-def onWake():
-    print("sensor detected motion! Wake the screen!")
-
-# def displayScreens():
-
-#     displayWindows()
-
-#     # displayWindowTwo()
-
-#     # frame_switcher = FrameSwitcher()
-#     # frame_switcher.mainloop()
-
-#     # elevator_reduction = ElevatorReductionWindows()
-#     # elevator_reduction.mainloop()
-    
 
 def main():
 
     window = tk.Tk()
 
-    # # Continous loop to wait for motion to be detected
-    # print("checking for motion")
-    # checkForMotion()
-    # # Once we have motion, go into initial procedure to wake up the screen
-    # onWake()
-
-    #Create the UI to display for the userd
-
-    displayAndSwitchWindows(window) 
-
-    # displayWindowOne()
-
-    # displayWindowTwo()
-    
+    displayAndSwitchWindows(window)
 
 if __name__ == '__main__':
     main()
